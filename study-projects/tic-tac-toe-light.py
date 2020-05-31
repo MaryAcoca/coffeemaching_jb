7280ad3c9a18159e5f76ac8669259549cc60c565
Mariia Acoca <39969264+MaryAcoca@users.noreply.github.com>
2020-05-31 21:11:58 +0200

    tictactie2

--- study-projects/tic-tac-toe-light.py
+++ study-projects/tic-tac-toe-light.py
@@ -1,127 +1,68 @@
 
+import string
+grid_input = list(input("Enter cells: "))
+playing_grid = [grid_input[:3], grid_input[3:6], grid_input[6:]]
+game_complete = "_" not in playing_grid
 
- 
-def print_table():
-    line1 = turn [0:3]
-    line1_spaces = " ".join(line1)
-    line2 = turn [3:6]
-    line2_spaces = " ".join(line2)
-    line3 = turn [6:]
-    line3_spaces = " ".join(line3)
+# def win_condition(line):
+#     if set(line) == {"X"}:
+#         return "X"
+#     if set(line) == {"O"}:
+#         return "O"
+#     else:
+#         return False
 
-    print(
-f"""
----------
-| {line1_spaces} |
-| {line2_spaces} |
-| {line3_spaces} |
----------""")
+def print_grid():
+    print("---------")
+    print("| " + " ".join(playing_grid[0]) + " |")
+    print("| " + " ".join(playing_grid[1]) + " |")
+    print("| " + " ".join(playing_grid[2]) + " |")
+    print("---------")
 
-def coordinates_check():
-    if 1 1:
-        if 
-
-
-def input_x():
-    if turn.startswith("XXX"):
-        return ("X wins")
-    elif turn.endswith("XXX"):
-        return ("X wins")
-    elif turn[3] == "X" and turn[4] == "X" and turn[5] == "X":
-        return ("X wins")
-    elif turn[0] == "X" and turn[3] == "X" and turn[6] == "X":
-        return ("X wins")           
-    elif turn[1] == "X" and turn[4] == "X" and turn[7] == "X":
-        return ("X wins")
-    elif turn[2] == "X" and turn[5] == "X" and turn[8] == "X":
-        return ("X wins")   
-    elif turn[0] == "X" and turn[4] == "X" and turn[8] == "X":
-        return ("X wins")
-    elif turn[2] == "X" and turn[4] == "X" and turn[6] == "X":
-        return ("X wins")
-
-
-def input_o():
-    if turn.startswith("OOO"):
-        return ("O Wins")
-    elif turn.endswith("OOO"):
-        return ("O Wins")
-    elif turn[3] == "O" and turn[4] == "O" and turn[5] == "O":
-        return ("O Wins")
-    elif turn[0] == "O" and turn[3] == "O" and turn[6] == "O":
-        return ("O Wins")
-    elif turn[1] == "O" and turn[4] == "O" and turn[7] == "O":
-        return ("O Wins")
-    elif turn[2] == "O" and turn[5] == "O" and turn[8] == "O":
-        return ("O Wins")
-    elif turn[0] == "O" and turn[4] == "O" and turn[8] == "O":
-        return ("O Wins")
-    elif turn[2] == "O" and turn[4] == "O" and turn[6] == "O":
-        return ("O Wins")
-
-def draw():
-    if turn.count("X") == 5 and turn.count("O") == 4 or turn.count("X") == 4 and turn.count("O") == 5:
-        if not input_x () and not input_o():
-            return("Draw")
+print_grid()
 
 while True:
-    turn = input("Enter cels:")
-    print_table()
-    enter_the_coordinates = input("Enter the coordinates:")
-
-
-    if input_x():
-        if input_o():
-            print_table()
-            print()
-            print ('Impossible')
-            break
-
-        else:
-            print_table()
-            print()
-            print ("X wins")
-            break
-
-    elif input_o():
-        print_table()
-        if input_x():
-            print_table()
-            print()
-            print ('Impossible')
-            break
-
-        else:
-            print_table()
-            print()
-            print ("O wins")
-            break
-        
-    elif turn.count("X") - turn.count("O") >= 2:
-        print_table()
-        print()
-        print ('Impossible')
+    input_coord = input("Enter the coordinates: ").split()
+    if any(coord not in string.digits for coord in input_coord):
+        print("You should enter numbers!")
+        continue
+    int_coord = [int(coord) for coord in input_coord ]
+    if any(coord < 1 or coord > 3 for coord in int_coord):
+        print("Coordinates should be from 1 to 3!")
+        continue
+    grid_coord = [-1 * (int_coord[1] - 3), int_coord[0] - 1]
+    if playing_grid[grid_coord[0]][grid_coord[1]] != "_":
+        print("This cell is occupied! Choose another one!")
+        continue
+    else:
+        playing_grid[grid_coord[0]][grid_coord[1]] = "X"
+    print_grid()
+    if game_complete:
         break
-
-    elif turn.count("O") - turn.count("X") >= 2:
-        print_table()
-        print()
-        print ('Impossible')
-        break
-
-    elif draw():
-        print_table()
-        print()
-        print("Draw")
-        break
-
-    elif not input_x() and not input_o() and not draw():
-        print_table()
-        print()
-        print("Game not finished")
-        break
-    
-   
-
    
   
+# # Defining the other possible winning lines
+# column_0 = [row[0] for row in playing_grid]
+# column_1 = [row[1] for row in playing_grid]
+# column_2 = [row[2] for row in playing_grid]
+# diagonal_0 = [row[count] for count, row in enumerate(playing_grid)]
+# diagonal_1 = [row[count + (count - 1) * -2] for count, row in enumerate(playing_grid)]
+
+# # Check of all lines for the given win condition
+# winning_lines = [playing_grid[0], playing_grid[1], playing_grid[2], column_0,
+#                  column_1, column_2, diagonal_0, diagonal_1]
+# win_check = [win_condition(line) for line in winning_lines]
+#
+# if abs(grid_input.count("X") - grid_input.count("O")) > 1:
+#     print("Impossible")
+# elif "X" in win_check and "O" in win_check:
+#     print("Impossible")
+# elif "X" in win_check:
+#     print("X wins")
+# elif "O" in win_check:
+#     print("O wins")
+# elif set(win_check) == {False}:
+#     if game_complete == True:
+#         print("Draw")
+#     elif game_complete == False:
+#         print("Game not finished")
